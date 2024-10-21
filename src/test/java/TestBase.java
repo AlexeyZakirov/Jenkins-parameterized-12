f71592203accde6ee9af900fb72540fcee6990ad
AlexeyZakirov <tinwhipcf@gmail.com>
2024-10-21 13:16:43 +0900

    added properties size and version

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void setUp() {
+        String remoteUrl = System.getProperty("remoteUrl");
         Configuration.browser = System.getProperty("browser","chrome");
         Configuration.browserVersion = System.getProperty("version");
         Configuration.browserSize = System.getProperty("browserSize");
@@ -25,7 +26,8 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = String.format("https://user1:1234@%s/wd/hub", remoteUrl);
 
 
     }
